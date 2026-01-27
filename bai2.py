94d72f28c9dcb15c70c33a2f0abe91dc82b89f4d
hongchik <hongichik321@gmail.com>
2026-01-27 09:43:18 +0700

    update

--- bai2.py
+++ bai2.py
@@ -1,9 +1,15 @@
+sochan = 0
+sole = 0
+
 while True:
     so = int(input("Nhập số nguyên dương n: "))
     if so == 0:
         print("Kết thúc chương trình.")
         break
     if so %2 == 0:
-        print(f"{so} là số chẵn.")
+        sochan += 1
     else:
         print(f"{so} là số lẻ.")
+        sole += 1
+print(f"Tổng số chẵn đã nhập: {sochan}")
+print(f"Tổng số lẻ đã nhập: {sole}")
