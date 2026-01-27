06ad11e96d4049765536257090a65e3ce94f17ec
daw <nguyenhoangvinh19072006@gmail.com>
2026-01-27 11:28:33 +0700

    baikt1

--- NguyenHoangVinh_24DH260188/baikt1.py
+++ NguyenHoangVinh_24DH260188/baikt1.py
@@ -1,1 +1,12 @@
-print("a")
+tong =0
+while True:
+    n = int(input("Nhập số nguyên: "))
+    if n > 0:
+        break
+for i in range(1, n - 1):
+    if n%i ==0:
+        tong += i
+if tong ==n:
+    print(n,"là số hoàn hảo")
+else:
+    print(n,"không phải là số hoàn hảo")
