e81009ffb3032e270e91137072de589196c9946b
khanhhung-dn <duonghungz23456789@gmail.com>
2026-01-27 11:02:42 +0700

    baikt1

---
+++ 24DH260081_DuongNguyenKhanhHung/baikt1.py
@@ -0,0 +1,11 @@
+n = int(input("Nhập số nguyên N: "))
+
+tong = 0
+for i in range(1, n):
+    if n % i == 0:
+        tong += i
+
+if tong == n:
+    print(n, "là số hoàn hảo")
+else:
+    print(n, "không phải là số hoàn hảo")
