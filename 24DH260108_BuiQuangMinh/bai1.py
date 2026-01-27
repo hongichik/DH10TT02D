630831a803c3164751f17bf48594e46886d64941
minh-nhom8 <minhbuoi25022006@gmail.com>
2026-01-27 09:45:41 +0700

    Bai tap 2

--- 24DH260108_BuiQuangMinh/bai1.py
+++
@@ -1,9 +0,0 @@
-dtb =float(input("Nhập điểm trung bình: "))
-if dtb>=8:
-    print("Bạn đạt học lực giỏi")
-elif dtb >= 6.5:
-    print("Bạn đạt học lực khá")
-elif dtb >=5:
-    print("Bạn đạt học lực trung bình")
-else:
-    print("Bạn đạt học lực yếu")
