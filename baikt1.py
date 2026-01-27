69955be2e21e71a6cf7d80d1fab76d80738fb1cc
anhsairoi206 <hieu2like1@gmail.com>
2026-01-27 21:31:00 +0700

    24DH260055_Nguyen-Van-Hieu

---
+++ baikt1.py
@@ -0,0 +1,11 @@
+
+N = int(input("Nhập số nguyên N: "))
+
+tong = 0
+for i in range(1, N):
+    if N % i == 0:
+        tong += i
+if tong == N:
+    print(N, "là số hoàn hảo!")
+else:
+    print(N, "không phải là số hoàn hảo!")
