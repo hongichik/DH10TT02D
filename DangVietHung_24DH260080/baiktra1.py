f4b30c83da7e9cfeda5557e7e3aed710b6f2eafd
Huy-nhom10 <tranquochuyvn3@gmail.com>
2026-01-27 23:33:40 +0700

    baiktra1.py

---
+++ DangVietHung_24DH260080/baiktra1.py
@@ -0,0 +1,10 @@
+N = int(input("Nhập số nguyên N: "))
+tong_uoc = 0
+for i in range(1, N):
+    if N % i == 0:
+        tong_uoc += i
+
+if tong_uoc == N:
+    print(N, "là số hoàn hảo")
+else:
+    print(N, "không phải là số hoàn hảo")
