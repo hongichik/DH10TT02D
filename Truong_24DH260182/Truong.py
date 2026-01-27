8eac5a48c36aabece7670d9ded58d24269ab19f3
Truong-nhom1 <ductruong6116@gmail.com>
2026-01-27 09:14:31 +0700

    bai 1

--- Truong_24DH260182/Truong.py
+++ Truong_24DH260182/Truong.py
@@ -1,1 +1,9 @@
-print("abc")
+dtb = float(input("Nhap diem tb"))
+if dtb >=8:
+    print("hoc luc gioi")
+elif dtb >=6.5:
+    print("hoc luc kha")
+elif dtb >=5:
+    print("hoc luc trung binh")
+else:
+    print("hoc luc yeu")
