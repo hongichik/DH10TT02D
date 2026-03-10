770a4e20099ecb8cf80e35927e925e4784c97215
Lý Mạnh Chiến <chienly45678@gmail.com>
2026-03-10 11:20:39 +0700

    Baitap3_1

---
+++ LyManhChien_Nhom6_24DH260192/Baitap3_2.py
@@ -0,0 +1,10 @@
+n = 5
+khoangtrang = n-1
+while khoangtrang >= 0:
+    print(khoangtrang)
+
+    for i in range(n):
+        print ("",end ="")
+    for k in range(2*i+1):
+        print("*",end="")
+    print()
