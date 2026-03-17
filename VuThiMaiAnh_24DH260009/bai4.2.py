ec64ce7b32d068eb84effcd7dc64490d1d71a3d2
Vũ Thị Mai Anh <vumai31122006@gmail.com>
2026-03-17 09:58:55 +0700

    bai4.2

---
+++ VuThiMaiAnh_24DH260009/bai4.2.py
@@ -0,0 +1,13 @@
+
+person = {
+    "ten": "mai anh",
+    "tuoi": 20,
+    "nghe nghiep": "sinh vien"
+}
+
+person["email"] = "maianh@gmail.com"
+
+person["tuoi"] = 25
+
+del person["nghe nghiep"]
+print("Thong tin ca nhan:", person)
