4069488ca4fb6640ed0e64346a12a96b08333e48
vananh <kopvon5@gmail.com>
2026-03-17 09:18:09 +0700

    vananh

---
+++ PhamVanAnh_Nhom6_24DH260005/bai4_2.py
@@ -0,0 +1,7 @@
+thong_tin={"tên":"Anh","tuổi":20,"nghề nghiệp":"sinh viên"}
+thong_tin["email"]="vangrp@gmail.com"
+print(thong_tin)
+thong_tin ["tuổi"]=25
+
+del thong_tin["nghề nghiệp"]
+print(thong_tin)
