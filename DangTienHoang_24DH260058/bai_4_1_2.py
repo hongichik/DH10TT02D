927d482af2240275adef943616f1aa7328c9083e
Dang Tien Hoang 2006 <danghoang010606@gmail.com>
2026-03-17 09:33:47 +0700

    bai4

---
+++ DangTienHoang_24DH260058/bai_4_1_2.py
@@ -0,0 +1,12 @@
+person = {
+    "ten": "Long Van An",
+    "tuoi": 20,
+    "nghe_nghiep": "Sinh vien"
+}
+
+person["email"] = "longvanan@gmail.com"
+
+person["tuoi"] = 25
+
+person.pop("nghe_nghiep")
+print(person)
