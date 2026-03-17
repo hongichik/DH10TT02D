a777315b9d27c614ef59522d2ac6fc6e668582dd
minh-nhom9 <hoangminh20060916@gmail.com>
2026-03-17 09:53:48 +0700

    bai4_2.py

---
+++ Minh_24DH260112/bai4_2.py
@@ -0,0 +1,8 @@
+Thong_tin={"name":"Nguyen Hoang Minh","age":20,"job":"sinh vien"}
+print(Thong_tin)
+Thong_tin["email"] = "hoangminh20060916@gmail.com"
+print(Thong_tin)
+Thong_tin["age"] = 21
+print(Thong_tin)
+Thong_tin.pop("job")
+print(Thong_tin)
