da1e4dec982df9878689e3b454a5cda39830ee1a
Ngo Dinh Dai <dinhdai2101@gmail.com>
2026-03-17 11:12:31 +0700

    Thong tin sinh vien

---
+++ NgoDinhDai_24DH260030/bai4-3.py
@@ -0,0 +1,15 @@
+student = {
+    "id": "SV001",
+    "name": "An",
+    "age": 20,
+    "gpa": 8.5
+}
+
+print("Ban đầu:", student)
+student["nghanh"] = "CNTT"
+print("Sau khi thêm nghanh :", student)
+student["gpa"] = student["gpa"] + 0.5
+print("Sau khi tăng GPA:", student)
+
+for key, value in student.items():
+    print( "|",key, ":", value,"|")
