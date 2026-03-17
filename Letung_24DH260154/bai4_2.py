5fa8c60ceab940bb5446dfafc0fbf031e83cac8d
letung-nhom7 <letung19022006@gmail.com>
2026-03-17 09:51:43 +0700

    bai4_2.py

---
+++ Letung_24DH260154/bai4_2.py
@@ -0,0 +1,8 @@
+Thong_tin={"name":"Le Tung","age":20,"job":"Ti phu"}
+print(Thong_tin)
+Thong_tin["email"]="letung19022006@gmail.com"
+print(Thong_tin)
+Thong_tin["age"]=100
+print(Thong_tin)
+Thong_tin.pop("job")
+print(Thong_tin)
