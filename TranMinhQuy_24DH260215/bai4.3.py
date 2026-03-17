f0a0328c772c7b4132d780c8eea7f0be3b3a2f0c
tranminhquy006 <tranminhquy006@gmail.com>
2026-03-17 11:09:28 +0700

    bai4.3.py

--- TranMinhQuy_24DH260215/bai4.3.py
+++ TranMinhQuy_24DH260215/bai4.3.py
@@ -1,13 +1,6 @@
-# Tạo hai danh sách
 list1 = [1, 2, 3]
 list2 = [4, 5, 6]
-
-# Ghép hai danh sách
 merged_list = list1 + list2
-
-# Nhân đôi tất cả phần tử trong danh sách
 result = [x * 2 for x in merged_list]
-
-# In kết quả
 print("Danh sách sau khi ghép:", merged_list)
 print("Danh sách sau khi nhân đôi:", result)
