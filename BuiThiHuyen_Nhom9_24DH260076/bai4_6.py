a3eb8b6e532536f9c8231ccedefadb88f3c8c9c2
Bui_Thi_Huyen_Nhom9 <huyenthu063008@gmail.com>
2026-03-21 20:10:41 +0700

    bt4.6

---
+++ BuiThiHuyen_Nhom9_24DH260076/bai4_6.py
@@ -0,0 +1,6 @@
+sdt = {"An":"0901234567","Binh":"0912345678","Cuong":"0923456789"}
+print("Danh sách sdt 3 bạn: ",sdt)
+print("Số điện thoại Bình: ",sdt["Binh"])
+del sdt["Cuong"]
+print("Xóa số Cường: ",sdt)
+print("Cập nhật danh bạ", sdt)
