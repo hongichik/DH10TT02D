e0e3bd8d0c7dd1171cffcea8007d9a7501518f11
Lý Mạnh Chiến <chienly45678@gmail.com>
2026-04-14 09:40:57 +0700

    baitap

---
+++ LyManhChien_Nhom6_24DH260192/24DH260192_lymanhchien_de1.py
@@ -0,0 +1,34 @@
+# a. Hàm nhập thông tin sinh viên
+def nhap():
+    MaSV = input("Nhap ma sinh vien: ")
+    ten = input("Nhap ten sinh vien: ")
+    diem = float(input("Nhap diem sinh vien: "))
+    return {"MaSV": MaSV, "ten": ten, "diem": diem}
+# b. Hàm tính điểm trung bình
+def DTB(sinhvien_list):
+    if not sinhvien_list:
+        return 0
+    tong_diem = sum(sv['diem'] for sv in sinhvien_list)
+    return tong_diem / len(sinhvien_list)
+# c. Hàm lọc sinh viên theo điểm
+def loc (sinhvien_list, diem = 5.0):
+    danhsachloc = [sv for sv in sinhvien_list if sv['diem'] >= diem]
+    return {"Danh sach sinh vien co diem lon hon hoac bang 5.0": danhsachloc}
+
+# d. Chương trình chính
+danh_sach_sv = []
+print("Nhap thong tin 3 sinh vien")
+for i in range(3):
+    print("Sinh vien: ", i+1)
+    sv = nhap()
+    danh_sach_sv.append(sv)
+dtb_chung = DTB(danh_sach_sv)
+print("Diem trung binh cua tat ca sinh vien: ", dtb_chung)
+
+print("Danh sach sinh vien co diem lon hon hoac bang 7.0:")
+sv_gioi = loc(danh_sach_sv,7.0)
+
+if not sv_gioi:
+    print("Khong co sinh vien gioi nao !")
+else:
+    print(f"- {sv['ten']} (MSV: {sv['MaSV']}) với {sv['diem']} điểm")
